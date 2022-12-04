2e63cebb206d98a797a2fdfbada4a43191cfabd2
David Savoie <dhsavoie@gmail.com>
2022-12-04 16:21:47 -0500

    finished routes to support feature to add review

--- flask-app/src/professors/professors.py
+++ flask-app/src/professors/professors.py
@@ -5,57 +5,57 @@ from src import db
 
 professors = Blueprint('professors', __name__)
 
-# Get all the professors from the database
-@professors.route('/professors', methods=['GET'])
-def get_professors():
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    # use cursor to query the database for a list of professors
-    cursor.execute('select professorCode, professorName, professorVendor from professors')
-
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
+# this is a route for listing all of the departments
+@professors.route('/departments', methods=['GET'])
+def get_departments():
+    cursor = db.get_db().cursor() # get a cursor object from the database
+    cursor.execute('select DepartmentName from Department') # get all department names
+    row_headers = [x[0] for x in cursor.description]
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response # return the data as a JSON response
 
-    return jsonify(json_data)
-
-# get the top 5 professors from the database
-@professors.route('/top5professors')
-def get_most_pop_professors():
+# this is a route for listing all of the professors in a department
+@professors.route('/dept_profs', methods=['POST'])
+def get_dept_profs():
+    dept = request.form # get the data from the request
     cursor = db.get_db().cursor()
-    query = '''
-        SELECT p.professorCode, professorName, sum(quantityOrdered) as totalOrders
-        FROM professors p JOIN orderdetails od on p.professorCode = od.professorCode
-        GROUP BY p.professorCode, professorName
-        ORDER BY totalOrders DESC
-        LIMIT 5;
-    '''
-    cursor.execute(query)
-       # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
+    dept_name = dept['DepartmentName'] # get the department name from the form
+    # execute query to get all professors in the department
+    cursor.execute('select FirstName, LastName from Professor where Department = %s', (dept_name))
+    row_headers = [x[0] for x in cursor.description]
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
-    return jsonify(json_data)
+# this is a route to get all the courses which a professor teaches
+@professors.route('/prof_courses', methods=['POST'])
+def get_prof_courses():
+    prof = request.form # get the data from the request
+    cursor = db.get_db().cursor()
+    prof_fname = prof['Name'].split()[0]
+    prof_lname = prof['Name'].split()[1]
+    # get professor id from first and last name
+    cursor.execute('select ProfessorID from Professor where FirstName = %s and LastName = %s', (prof_fname, prof_lname))
+    ProfessorID = cursor.fetchone()[0]
+    # execute query to get all courses taught by the professor
+    cursor.execute('select CourseName, CourseSection from Course where Professor = %s', (ProfessorID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -5,28 +5,15 @@ from src import db
 
 students = Blueprint('students', __name__)
 
-# Get all students from the DB
-@students.route('/students', methods=['GET'])
-def get_students():
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from Student')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-
+# this is a post route for adding a new review
 @students.route('/add_review', methods=['POST'])
 def add_review():
-    # will need to grab professor's id from their name as entered here
+    # get the data from the request
     review = request.form
-    current_app.logger.info(review)
-    cursor = db.get_db().cursor()
+    current_app.logger.info(review) # log the data to the console
+    cursor = db.get_db().cursor() # get a cursor object from the database
+
+    # get the data from the form
     ReviewContent = review['ReviewContent']
     Class = review['Class']
     WorkloadRating = review['WorkloadRating']
@@ -34,30 +21,18 @@ def add_review():
     EngagementRating = review['EngagementRating']
     StudentReviewer = review['StudentReviewer']
     ProfessorReviewed = review['ProfessorReviewed']
+
+    # get the professor's ID from their name
     prof_fname = ProfessorReviewed.split()[0]
     prof_lname = ProfessorReviewed.split()[1]
     cursor.execute('select ProfessorID from Professor where FirstName = %s and LastName = %s', (prof_fname, prof_lname))
     ProfessorID = cursor.fetchone()[0]
+
+    # insert review data into the database
     query = f"""INSERT INTO Review (ReviewContent, Class, WorkloadRating, DifficultyRating, EngagementRating, StudentReviewer, 
     ProfessorReviewed) VALUES ('{ReviewContent}', '{Class}', {WorkloadRating}, {DifficultyRating}, {EngagementRating}, 
-    {StudentReviewer}, '{ProfessorID}')"""
+    {StudentReviewer}, {ProfessorID})"""
     current_app.logger.info(query)
     cursor.execute(query)
     db.get_db().commit()
     return "Success! Review added"
-
-"""
-# Get student detail for student with particular userID
-@students.route('/students/<userID>', methods=['GET'])
-def get_student(userID):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from student where StudentID = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response"""
