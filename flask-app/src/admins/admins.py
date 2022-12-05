f83e913eec4d593bf8a98600f9b6abfdb265ebe0
darayus-gorimaar <dgorimaar@gmail.com>
2022-12-05 15:33:25 -0500

    added routes

--- flask-app/src/admins/admins.py
+++ flask-app/src/admins/admins.py
@@ -5,57 +5,43 @@ from src import db
 
 admins = Blueprint('admins', __name__)
 
-# Get all the admins from the database
-@admins.route('/admins', methods=['GET'])
-def get_admins():
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    # use cursor to query the database for a list of admins
-    cursor.execute('select adminCode, adminName, adminVendor from admins')
-
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
+# this is a route for listing all of the departments
+@admins.route('/get_majors', methods=['GET'])
+def get_majors():
+    cursor = db.get_db().cursor() # get a cursor object from the database
+    cursor.execute('select Major from Major') # get all department names
+    row_headers = [x[0] for x in cursor.description]
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response # return the data as a JSON response
 
-    return jsonify(json_data)
+# # Get all the admins from the database
+# @admins.route('/admins', methods=['GET'])
+# def get_admins():
+#     # get a cursor object from the database
+#     cursor = db.get_db().cursor()
 
-# get the top 5 admins from the database
-@admins.route('/top5admins')
-def get_most_pop_admins():
-    cursor = db.get_db().cursor()
-    query = '''
-        SELECT p.adminCode, adminName, sum(quantityOrdered) as totalOrders
-        FROM admins p JOIN orderdetails od on p.adminCode = od.adminCode
-        GROUP BY p.adminCode, adminName
-        ORDER BY totalOrders DESC
-        LIMIT 5;
-    '''
-    cursor.execute(query)
-       # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
+#     # use cursor to query the database for a list of admins
+#     cursor.execute('select adminCode, adminName, adminVendor from admins')
 
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
+#     # grab the column headers from the returned data
+#     column_headers = [x[0] for x in cursor.description]
 
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
+#     # create an empty dictionary object to use in 
+#     # putting column headers together with data
+#     json_data = []
 
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
+#     # fetch all the data from the cursor
+#     theData = cursor.fetchall()
 
-    return jsonify(json_data)
+#     # for each of the rows, zip the data elements together with
+#     # the column headers. 
+#     for row in theData:
+#         json_data.append(dict(zip(column_headers, row)))
+
+#     return jsonify(json_data)
