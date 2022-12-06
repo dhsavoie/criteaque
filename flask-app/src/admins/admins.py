b4b167f8f9eb44c36d8e7ed0219a517ce98d77ec
darayus-gorimaar <dgorimaar@gmail.com>
2022-12-06 15:55:40 -0500

    add_student update

--- flask-app/src/admins/admins.py
+++ flask-app/src/admins/admins.py
@@ -6,10 +6,12 @@ from src import db
 admins = Blueprint('admins', __name__)
 
 # this is a route for listing all of the departments
+
+
 @admins.route('/get_majors', methods=['GET'])
 def get_majors():
-    cursor = db.get_db().cursor() # get a cursor object from the database
-    cursor.execute('select Major from Major') # get all department names
+    cursor = db.get_db().cursor()  # get a cursor object from the database
+    cursor.execute('select Major from Major')  # get all department names
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -18,17 +20,19 @@ def get_majors():
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
-    return the_response # return the data as a JSON response
+    return the_response  # return the data as a JSON response
 
     # this is a post route for adding a new user
+
+
 @admins.route('/add_student', methods=['POST'])
 def add_student():
     # get the data from the request
     student = request.form
-    current_app.logger.info(student) # log the data to the console
-    cursor = db.get_db().cursor() # get a cursor object from the database
+    current_app.logger.info(student)  # log the data to the console
+    cursor = db.get_db().cursor()  # get a cursor object from the database
 
-# insert into Student (StudentID, FirstName, LastName, email, Year, Username, Password, Major) values 
+# insert into Student (StudentID, FirstName, LastName, email, Year, Username, Password, Major) values
 # ('008985560', 'Lyell', 'Goscar', DEFAULT, 2, DEFAULT, 'dIGdTj8Mg5N', 'Computer Science');
 
     # get the data from the form
