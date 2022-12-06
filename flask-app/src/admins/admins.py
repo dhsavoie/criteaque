c3eb8985379b348faa1bd9831d51d06f682a77c6
darayus-gorimaar <dgorimaar@gmail.com>
2022-12-06 17:31:40 -0500

    added add_prof and get_depts

--- flask-app/src/admins/admins.py
+++ flask-app/src/admins/admins.py
@@ -6,8 +6,6 @@ from src import db
 admins = Blueprint('admins', __name__)
 
 # this is a route for listing all of the departments
-
-
 @admins.route('/get_majors', methods=['GET'])
 def get_majors():
     cursor = db.get_db().cursor()  # get a cursor object from the database
@@ -22,30 +20,44 @@ def get_majors():
     the_response.mimetype = 'application/json'
     return the_response  # return the data as a JSON response
 
-    # this is a post route for adding a new user
+# this is a route for listing all of the departments
+@admins.route('/get_departments', methods=['GET'])
+def get_departments():
+    cursor = db.get_db().cursor()  # get a cursor object from the database
+    # get all department names
+    cursor.execute('select DepartmentName from Department')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response  # return the data as a JSON response
 
-
+# this is a post route for adding a new user
 @admins.route('/add_student', methods=['POST'])
 def add_student():
     # get the data from the request
-    student = request.form
-    current_app.logger.info(student)  # log the data to the console
+    studentAdd = request.form
+    current_app.logger.info(studentAdd)  # log the data to the console
     cursor = db.get_db().cursor()  # get a cursor object from the database
 
-    # # get the data from the form
-    # FirstName = student['FirstName']
-    # LastName = student['LastName']
-    # StudentID = student['StudentID']
-    # Year = student['Year']
-    # Major = student['Major']
-    # Password = student['Password']
+    # get the data from the form
+    FirstName = studentAdd['FirstName']
+    LastName = studentAdd['LastName']
+    StudentID = studentAdd['StudentID']
+    Year = studentAdd['Year']
+    Major = studentAdd['Major']
+    Password = studentAdd['Password']
 
     # insert review data into the database
-    # query = f"""INSERT INTO Student (FirstName, LastName, StudentID, Year,  Major, Password)
-    # VALUES ('{FirstName}', '{LastName}', {StudentID}, {Year}, {Major}, {Password})"""
+    query = f"""INSERT INTO Student (FirstName, LastName, StudentID, Year,  Major, Password)
+    VALUES ('{FirstName}', '{LastName}', '{StudentID}', '{Year}', '{Major}', '{Password}')"""
 
-    query = f"""INSERT INTO Student (FirstName, LastName, StudentID, Year,  Major, Password) 
-    VALUES ('Michael', 'Jordan', '0000023', '2', 'Computer Science' , "SpaceJam")"""
+    # query = f"""INSERT INTO Student (FirstName, LastName, StudentID, Year,  Major, Password)
+    # VALUES ('Red', 'Foreman', '0000070', '5', 'Computer Science' , 'Angry')"""
 
     current_app.logger.info(query)
     cursor.execute(query)
